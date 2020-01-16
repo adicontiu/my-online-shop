621a694e0216fe0bc00876aba4779da08a9a91b3
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-16 07:44:43 +0200

    Additional routing stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import {Homepage} from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+    <div>
+      <h1>HATS</h1>
+    </div>
+);
 
 function App() {
   return (
       <div>
-        <Homepage/>
+        <Switch>
+          <Route path="/shop/hats" component={HatsPage}/>
+          <Route path="/" component={Homepage}/>
+        </Switch>
       </div>
   );
 }
