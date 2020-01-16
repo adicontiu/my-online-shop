ffeee0be10cfba829f6a4f54cf2ce535dd4303ac
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-16 08:30:02 +0200

    shop component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Homepage} from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom';
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => (
     <div>
@@ -14,7 +15,8 @@ function App() {
       <div>
         <Switch>
           <Route path="/shop/hats" component={HatsPage}/>
-          <Route path="/" component={Homepage}/>
+          <Route exact path="/" component={Homepage}/>
+          <Route path="/shop" component={ShopPage}/>
         </Switch>
       </div>
   );
