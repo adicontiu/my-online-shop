a8e22ea1b816471828bd70915ae5322c1b02d051
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-16 08:40:55 +0200

    adding header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Homepage} from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
     <div>
@@ -13,6 +14,7 @@ const HatsPage = () => (
 function App() {
   return (
       <div>
+        <Header/>
         <Switch>
           <Route path="/shop/hats" component={HatsPage}/>
           <Route exact path="/" component={Homepage}/>
