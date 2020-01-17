474454cd191d3666f7bfa0835d57877551d7c281
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-17 08:10:13 +0200

    Adding other stuff

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import './App.css';
 import {Homepage} from './pages/homepage/homepage.component';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
 import SignInAndSignUp
   from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser2} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          }, () => console.log(this.state))
+          setCurrentUser2({
+            id: snapShot.id,
+            ...snapShot.data()
+          })
         });
 
-      } else {
-        this.setState({
-          currentUser: userAuth
-        }, () => console.log(this.state));
       }
+      setCurrentUser2(userAuth);
     });
   }
 
@@ -47,15 +39,22 @@ class App extends React.Component {
   render() {
     return (
         <div>
-          <Header currentUser={this.state.currentUser}/>
+          <Header/>
           <Switch>
             <Route exact path="/" component={Homepage}/>
             <Route path="/shop" component={ShopPage}/>
-            <Route path="/signin" component={SignInAndSignUp}/>
+            <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/"/>): (<SignInAndSignUp/>)}/>
           </Switch>
         </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser2: user => dispatch(setCurrentUser(user))
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {connect} from 'react-redux'; //higher order component that let's us modify our component to have access to things in redux
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
-
 import './header.styles.scss';
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({currentUser}) => (
     <div className="header">
@@ -19,9 +20,20 @@ const Header = ({currentUser}) => (
             :
             <Link className="option" to="/signin">SIGN IN</Link>
         }
+        <CartIcon/>
       </div>
 
     </div>
 );
 
-export default Header;
+//function to access the state!
+//state <- root state reducer
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+});
+
+//higher order components(connect) are just functions that take components as arguments and then return you a new subcomponent
+// 2 arguments:
+// -- the function that let's us access the state
+// --- the second function (optional)
+export default connect(mapStateToProps)(Header);
