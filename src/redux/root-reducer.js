181c5cc62b4507d53f06664d7e0a1208f894f161
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-18 15:27:31 +0200

    Store application state into our local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,20 @@
 //combines all of the reducers
 import {combineReducers} from 'redux';
-
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root', // from where do we want to start storing
+  storage,
+  whitelist: ['cart'] // which reducers we want to store
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+//store into cache
+export default persistReducer(persistConfig, rootReducer);
