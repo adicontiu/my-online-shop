3f0bdafc18bf69a8fb5fbf0c10ca7dcdacb5e9f8
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-18 08:17:13 +0200

    Adding cartitem in popup

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from "../cart-item/cart-item.component";
+import {connect} from 'react-redux';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
-      <div className="cart-items"/>
+      <div className="cart-items">
+        {cartItems.map(item => <CartItem key={item.id} item={item}/>)}
+      </div>
       <CustomButton>Go to Checkout</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+      cartItems: state.cart.cartItems
+    }
+);
+export default connect(mapStateToProps, null)(CartDropdown);
