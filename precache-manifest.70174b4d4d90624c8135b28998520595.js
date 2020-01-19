4066bb7b609933d9174fe78f958ccbe8906e39ea
adina.contiu <adina.contiu@3pillarglobal.com>
2020-01-19 08:13:34 +0200

    Updates

---
+++ precache-manifest.70174b4d4d90624c8135b28998520595.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07afbbf8d243a77803391465ccf1ed8",
+    "url": "/my-online-shop/index.html"
+  },
+  {
+    "revision": "d8309287c69a6b6156e6",
+    "url": "/my-online-shop/static/css/main.be5af287.chunk.css"
+  },
+  {
+    "revision": "535c2c1ecdcc6d8a8483",
+    "url": "/my-online-shop/static/js/2.0ca5271f.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/my-online-shop/static/js/2.0ca5271f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8309287c69a6b6156e6",
+    "url": "/my-online-shop/static/js/main.a0cb00dd.chunk.js"
+  },
+  {
+    "revision": "963370a8923f9f2dc56e",
+    "url": "/my-online-shop/static/js/runtime-main.c65f2f3a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/my-online-shop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/my-online-shop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
